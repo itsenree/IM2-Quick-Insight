331c646bfd9ce783d4b2073da29bd2fe9be8f506
itsenree <75910282+itsenree@users.noreply.github.com>
2025-05-06 14:46:59 +0200

    yuh

---
+++ script.js
@@ -0,0 +1,30 @@
+async function init() {
+}
+
+document.querySelectorAll(".loadCards").forEach((button) => {
+    button.addEventListener("click", async () => {
+        console.log(button.value);
+
+        const response = await fetch("https://tarotapi.dev/api/v1/cards/random?n=" + button.value);
+        const data = await response.json();
+        
+        const cards = data.cards;
+        const selectedData = cards.map(card => ({
+            name_short: card.name_short,
+            name: card.name,
+            type: card.type,
+            value: card.value_int,
+            meaning_rev: card.meaning_rev,
+            meaning_up: card.meaning_up
+        }));
+
+        const meaningsUp = selectedData.map(card => card.meaning_up);
+
+        console.log(selectedData);
+        // console.log(data);
+    })
+})
+
+
+
+// await init();
